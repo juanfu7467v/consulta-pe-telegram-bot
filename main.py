2db1c36fcfee51bc17549e8a1dcc3c77379023e8
juanfu7467v <movistarcervise@gmail.com>
2025-09-24 17:08:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -189,5 +189,4 @@ def startup():
         print("❌ Error iniciando Telethon:", e)
 
 if __name__ == "__main__":
-    print(f"🚀 App corriendo en http://0.0.0.0:{PORT} (PUBLIC_URL={PUBLIC_URL})")
-    app.run(host="0.0.0.0", port=PORT)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
